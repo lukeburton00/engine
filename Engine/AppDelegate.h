fd491db4671d59f743989cd76c9176fe0be7b71b
Luke Burton <lukesburton19@gmail.com>
2022-12-04 22:11:15 -0700

    Initial Commit

---
+++ Engine/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Engine
+//
+//  Created by Luke Burton on 12/4/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
